07ffd13449667d133350671975377c97943659eb
Spen Hanley <spen@fullnerd.net>
2017-05-05 00:11:54 -0700

    updated sql script to configure database

--- spen.sql
+++ spen.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: May 03, 2017 at 05:33 PM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 5.6.30
+-- Host: localhost
+-- Generation Time: May 05, 2017 at 12:10 AM
+-- Server version: 5.7.18-0ubuntu0.16.04.1
+-- PHP Version: 7.0.15-0ubuntu0.16.04.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -21,30 +21,21 @@ SET time_zone = "+00:00";
 --
 
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `books`
 --
 
 CREATE TABLE `books` (
-  `id` int(11) NOT NULL,
-  `isbn` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
   `title` varchar(128) NOT NULL,
   `price` float NOT NULL,
-  `cover_art` varchar(256) NOT NULL,
+  `cover_art` varchar(256) NOT NULL DEFAULT 'cover.jpg',
   `author_name` varchar(11) NOT NULL,
-  `publisher_name` varchar(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
-  `username` varchar(64) NOT NULL,
-  `password` varchar(64) NOT NULL
+  `genre_id` int(11) NOT NULL,
+  `description` varchar(256) NOT NULL,
+  `short_desc` varchar(128) NOT NULL,
+  `book_quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -55,8 +46,67 @@ CREATE TABLE `users` (
 
 CREATE TABLE `genres` (
   `genre_id` int(11) NOT NULL,
-  `genre_name` varchar(64) NOT NULL,
+  `genre_name` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `genres`
+--
+
+INSERT INTO `genres` (`genre_id`, `genre_name`) VALUES
+(1, 'Science Fiction'),
+(2, 'Satire'),
+(3, 'Drama'),
+(4, 'Action and Adventure'),
+(5, 'Romance'),
+(6, 'Mystery'),
+(7, 'Horror'),
+(8, 'Self Help'),
+(9, 'Health'),
+(10, 'Guide'),
+(11, 'Travel'),
+(12, 'Children\'s'),
+(13, 'Religion, Spirituality and New Age'),
+(14, 'Science'),
+(15, 'History'),
+(16, 'Maths'),
+(17, 'Anthology'),
+(18, 'Poetry'),
+(19, 'Encyclopedias'),
+(20, 'Dictionaries'),
+(21, 'Comics'),
+(22, 'Art'),
+(23, 'Cookbooks'),
+(24, 'Diaries'),
+(25, 'Journals'),
+(26, 'Prayer Books'),
+(27, 'Series'),
+(28, 'Trilogy'),
+(29, 'Biographies'),
+(30, 'Autobiographies'),
+(31, 'Fantasy');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(64) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `first_name` varchar(64) NOT NULL,
+  `last_name` varchar(64) NOT NULL,
+  `user_image` varchar(128) DEFAULT 'avatar.jpg'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `password`, `first_name`, `last_name`, `user_image`) VALUES
+(1, 'spen', 'root', 'Spen', 'Hanley', 'avatar.jpg');
 
 --
 -- Indexes for dumped tables
@@ -66,34 +116,50 @@ CREATE TABLE `genres` (
 -- Indexes for table `books`
 --
 ALTER TABLE `books`
-  ADD PRIMARY KEY (`book_id`);
+  ADD PRIMARY KEY (`book_id`),
+  ADD KEY `genre_id` (`genre_id`);
+
+--
+-- Indexes for table `genres`
+--
+ALTER TABLE `genres`
+  ADD PRIMARY KEY (`genre_id`);
+
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
-
-ALTER TABLE `genres`
-  ADD PRIMARY KEY (`genre_id`);
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `unique` (`first_name`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
   MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT for table `genres`
 --
 ALTER TABLE `genres`
-  MODIFY `genre_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `genre_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `books`
+--
+ALTER TABLE `books`
+  ADD CONSTRAINT `books_ibfk_1` FOREIGN KEY (`genre_id`) REFERENCES `genres` (`genre_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
